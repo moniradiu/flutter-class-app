c67af759a1ac6213e9356b89b7e704f681044240
moniradiu <moniradiu@gmail.com>
2021-04-25 02:43:11 +0600

    Single_View_Scroll_Column

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.lightBlue,
       ),
       home: MyHomePage(title: 'Flutter Lecture-12'),
     );
@@ -36,43 +36,73 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(15),
+        scrollDirection: Axis.vertical,
         child: Column(
           children: [
-            TextField(
-              controller: textEditingController,
-              cursorColor: Colors.green,
-              cursorHeight: 30,
-              decoration: InputDecoration(
-                  contentPadding:
-                      EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                  fillColor: Colors.black12,
-                  filled: true,
-                  hintText: "Enter Your Name",
-                  hintStyle: TextStyle(color: Colors.deepPurple),
-                  labelText: "First Name",
-                  labelStyle: TextStyle(color: Colors.blue[20], fontSize: 20),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(color: Colors.orange, width: 2)),
-                  enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(color: Colors.green)),
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(color: Colors.red)),
-                  disabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                  prefixIcon: const Icon(
-                    Icons.location_city,
-                    color: Colors.green,
-                  ),
-                  suffixIcon: Icon(
-                    Icons.arrow_upward_outlined,
-                    color: Colors.green,
-                  )),
+            Container(
+              height: 150,
+              color: Colors.amber[100],
             ),
+            Container(
+              height: 150,
+              color: Colors.pink[100],
+            ),
+            Container(
+              height: 150,
+              color: Colors.purple[200],
+            ),
+            Container(
+              height: 150,
+              color: Colors.blueGrey[100],
+            ),
+            Container(
+              height: 150,
+              color: Colors.red[100],
+            ),
+            Container(
+              height: 150,
+              color: Colors.green[100],
+            ),
+            Container(
+              height: 150,
+              color: Colors.lime[100],
+            ),
+
+            // TextField(
+            //   controller: textEditingController,
+            //   cursorColor: Colors.green,
+            //   cursorHeight: 30,
+            //   decoration: InputDecoration(
+            //       contentPadding:
+            //           EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+            //       fillColor: Colors.black12,
+            //       filled: true,
+            //       hintText: "Enter Your Name",
+            //       hintStyle: TextStyle(color: Colors.deepPurple),
+            //       labelText: "First Name",
+            //       labelStyle: TextStyle(color: Colors.blue[20], fontSize: 20),
+            //       border: OutlineInputBorder(
+            //           borderRadius: BorderRadius.circular(10),
+            //           borderSide: BorderSide(color: Colors.orange, width: 2)),
+            //       enabledBorder: OutlineInputBorder(
+            //           borderRadius: BorderRadius.circular(10),
+            //           borderSide: BorderSide(color: Colors.green)),
+            //       focusedBorder: OutlineInputBorder(
+            //           borderRadius: BorderRadius.circular(10),
+            //           borderSide: BorderSide(color: Colors.red)),
+            //       disabledBorder: OutlineInputBorder(
+            //           borderRadius: BorderRadius.circular(10)),
+            //       prefixIcon: const Icon(
+            //         Icons.location_city,
+            //         color: Colors.green,
+            //       ),
+            //       suffixIcon: Icon(
+            //         Icons.arrow_upward_outlined,
+            //         color: Colors.green,
+            //       )),
+            // ),
           ],
         ),
       ),
