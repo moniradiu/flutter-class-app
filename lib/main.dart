09d724aec9d9d135889b82db6a539da889a6b3fc
moniradiu <moniradiu@gmail.com>
2021-04-25 02:51:44 +0600

    SingleChildScrollView Row

--- lib/main.dart
+++ lib/main.dart
@@ -36,74 +36,42 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(15),
-        scrollDirection: Axis.vertical,
-        child: Column(
-          children: [
-            Container(
-              height: 150,
-              color: Colors.amber[100],
-            ),
-            Container(
-              height: 150,
-              color: Colors.pink[100],
-            ),
-            Container(
-              height: 150,
-              color: Colors.purple[200],
-            ),
-            Container(
-              height: 150,
-              color: Colors.blueGrey[100],
-            ),
-            Container(
-              height: 150,
-              color: Colors.red[100],
-            ),
-            Container(
-              height: 150,
-              color: Colors.green[100],
-            ),
-            Container(
-              height: 150,
-              color: Colors.lime[100],
-            ),
-
-            // TextField(
-            //   controller: textEditingController,
-            //   cursorColor: Colors.green,
-            //   cursorHeight: 30,
-            //   decoration: InputDecoration(
-            //       contentPadding:
-            //           EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-            //       fillColor: Colors.black12,
-            //       filled: true,
-            //       hintText: "Enter Your Name",
-            //       hintStyle: TextStyle(color: Colors.deepPurple),
-            //       labelText: "First Name",
-            //       labelStyle: TextStyle(color: Colors.blue[20], fontSize: 20),
-            //       border: OutlineInputBorder(
-            //           borderRadius: BorderRadius.circular(10),
-            //           borderSide: BorderSide(color: Colors.orange, width: 2)),
-            //       enabledBorder: OutlineInputBorder(
-            //           borderRadius: BorderRadius.circular(10),
-            //           borderSide: BorderSide(color: Colors.green)),
-            //       focusedBorder: OutlineInputBorder(
-            //           borderRadius: BorderRadius.circular(10),
-            //           borderSide: BorderSide(color: Colors.red)),
-            //       disabledBorder: OutlineInputBorder(
-            //           borderRadius: BorderRadius.circular(10)),
-            //       prefixIcon: const Icon(
-            //         Icons.location_city,
-            //         color: Colors.green,
-            //       ),
-            //       suffixIcon: Icon(
-            //         Icons.arrow_upward_outlined,
-            //         color: Colors.green,
-            //       )),
-            // ),
-          ],
+      body: Container(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(15),
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              Container(
+                width: 150,
+                color: Colors.amber[100],
+              ),
+              Container(
+                width: 150,
+                color: Colors.pink[100],
+              ),
+              Container(
+                width: 150,
+                color: Colors.purple[200],
+              ),
+              Container(
+                width: 150,
+                color: Colors.blueGrey[100],
+              ),
+              Container(
+                width: 150,
+                color: Colors.red[100],
+              ),
+              Container(
+                width: 150,
+                color: Colors.green[100],
+              ),
+              Container(
+                width: 150,
+                color: Colors.lime[100],
+              ),
+            ],
+          ),
         ),
       ),
     );
