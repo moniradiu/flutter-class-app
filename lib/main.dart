d4ee5ba8e8dfb5f9c15be46ad129e48db2a6c5ec
moniradiu <moniradiu@gmail.com>
2021-04-25 04:31:38 +0600

    Listviwe using Builder And Separated

--- lib/main.dart
+++ lib/main.dart
@@ -30,50 +30,75 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   var textEditingController = TextEditingController();
+  List<Color> color = [
+    Colors.amber[100],
+    Colors.pink[100],
+    Colors.purple[200],
+    Colors.blueGrey[100],
+    Colors.blue[100],
+    Colors.red[100],
+    Colors.green[100],
+    Colors.lime[200],
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Container(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.all(15),
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            children: [
-              Container(
-                width: 150,
-                color: Colors.amber[100],
-              ),
-              Container(
-                width: 150,
-                color: Colors.pink[100],
-              ),
-              Container(
-                width: 150,
-                color: Colors.purple[200],
-              ),
-              Container(
-                width: 150,
-                color: Colors.blueGrey[100],
-              ),
-              Container(
-                width: 150,
-                color: Colors.red[100],
-              ),
-              Container(
-                width: 150,
-                color: Colors.green[100],
-              ),
-              Container(
-                width: 150,
-                color: Colors.lime[100],
-              ),
-            ],
-          ),
-        ),
+      //body: ListView.builder(
+      body: ListView.separated(
+        separatorBuilder: (context, index) {
+          return Divider(
+            thickness: 10,
+          );
+        },
+        physics: BouncingScrollPhysics(),
+        itemCount: 8,
+        itemBuilder: (context, index) {
+          return Container(
+            height: 150,
+            color: color[index],
+          );
+        },
       ),
     );
   }
 }
+
+//  child: SingleChildScrollView(
+//         padding: const EdgeInsets.all(15),
+//         scrollDirection: Axis.horizontal,
+//         child: Row(
+//           children: [
+//             Container(
+//               width: 150,
+//               color: Colors.amber[100],
+//             ),
+//             Container(
+//               width: 150,
+//               color: Colors.pink[100],
+//             ),
+//             Container(
+//               width: 150,
+//               color: Colors.purple[200],
+//             ),
+//             Container(
+//               width: 150,
+//               color: Colors.blueGrey[100],
+//             ),
+//             Container(
+//               width: 150,
+//               color: Colors.red[100],
+//             ),
+//             Container(
+//               width: 150,
+//               color: Colors.green[100],
+//             ),
+//             Container(
+//               width: 150,
+//               color: Colors.lime[100],
+//             ),
+//           ],
+//         ),
+//       ),
